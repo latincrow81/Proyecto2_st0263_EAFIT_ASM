beff25970e8a0661242a75d121e9c4dbe2e0eff2
Mauricio Escudero <mauricio.escudero@patientpop.com>
2023-05-24 22:34:41 -0500

    wip config

--- asm/app/services.py
+++ asm/app/services.py
@@ -7,8 +7,8 @@ config = dotenv_values(".env")
 DRY_RUN = int(config.get('DRY_RUN'))
 my_config = Config(
     region_name=config.get('REGION'),
-    aws_access_key_id=config.get('aws_access_key_id'),
-    aws_secret_access_key=config.get('aws_secret_access_key')
+    aws_access_key_id=config.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=config.get('AWS_SECRET_ACCESS_KEY')
 )
 
 ec2 = boto3.resource("ec2", config=my_config)
