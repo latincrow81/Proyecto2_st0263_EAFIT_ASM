b1d10a54a1161560283cc1fdee52921517382f98
Mauricio Escudero <mauricio.escudero@patientpop.com>
2023-05-25 20:25:41 -0500

    ready for debug boto3

--- asm/app/services.py
+++ asm/app/services.py
@@ -5,12 +5,15 @@ from dotenv import dotenv_values
 config = dotenv_values(".env")
 
 DRY_RUN = int(config.get('DRY_RUN'))
+my_config = Config(
+    region_name=config.get('REGION')
+)
 
 _client = boto3.client('ec2',
                        aws_access_key_id=config.get('AWS_ACCESS_KEY_ID'),
-                       aws_secret_access_key=config.get('AWS_SECRET_ACCESS_KEY'),
-                       region_name=config.get('REGION'))
-ec2 = boto3.resource("ec2")
+                       aws_secret_access_key=config.get('AWS_SECRET_ACCESS_KEY')
+                       )
+ec2 = boto3.resource("ec2", config=my_config)
 
 
 def crear_instancia():
